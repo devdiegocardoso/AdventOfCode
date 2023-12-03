439d4681ffdc915abfde7a9221b1c85bbf57c224
Diego Cardoso <dev.diegocardoso@outlook.com>
2023-12-03 18:25:55 +0100

    Template modified

--- year/template/aocYYYYDD.py
+++ year/template/aocYYYYDD.py
@@ -1,20 +1,95 @@
+"""
+[summary]
+"""
 import pathlib
 import sys
 
+
 def parse(puzzle_input):
+    """
+    Parse the puzzle input into a list of integers.
+
+    Args:
+        puzzle_input (str): The puzzle input to be parsed.
+
+    Returns:
+        list: A list of integers parsed from the puzzle input.
+    """
     return [int(line) for line in puzzle_input.split()]
 
 def parse_list(puzzle_input):
-    list_tmp = [line for line in puzzle_input.split()]
+    """
+    Parse the puzzle input into a list of tuples.
+
+    The puzzle input is expected to be a string containing pairs of values. 
+    Each pair consists of a string followed by an integer. The function splits the input string, 
+    converts the second value of each pair to an integer, and returns a list of tuples where the 
+    first element of each tuple is the string and the second element is the corresponding integer.
+
+    Args:
+        puzzle_input (str): The puzzle input to be parsed.
+
+    Returns:
+        list: A list of tuples, where each tuple contains a string and an integer 
+        parsed from the puzzle input.
+    """
+
+    list_tmp = list(puzzle_input.split())
     return [(list_tmp[i], int(list_tmp[i+1])) for i in range(0,len(list_tmp),2)]
 
 def part_one(numbers):
+    """
+    Calculate the solution for part one of the Advent of Code puzzle.
+
+    This function takes a list of numbers as input and should return the solution 
+    for part one of the puzzle. The specific behavior and implementation details 
+    of this function are not provided in the code snippet.
+
+    Args:
+        numbers (list): A list of numbers.
+
+    Returns:
+        None: The function does not return any value.
+    """
+
     return
 
 def part_two(numbers):
+    """
+    Calculate the solution for part two of the Advent of Code puzzle.
+
+    This function takes a list of numbers as input and should return the solution for part two 
+    of the puzzle. The specific behavior and implementation details of this function are not 
+    provided in the code snippet.
+
+    Args:
+        numbers (list): A list of numbers.
+
+    Returns:
+        None: The function does not return any value.
+    """
+
     return
 
 def solve(puzzle_input):
+    """
+    Solve the Advent of Code puzzle.
+
+    This function takes the puzzle input as input and returns the solutions for both 
+    part one and part two of the puzzle. It first parses the puzzle input into a list 
+    of numbers using the `parse` function. Then, it calculates the solution for part 
+    one using the `part_one` function and the parsed numbers. Finally, it calculates 
+    the solution for part two using the `part_two` function and the parsed numbers. 
+    The solutions for both parts are returned as a tuple.
+
+    Args:
+        puzzle_input (str): The puzzle input to be solved.
+
+    Returns:
+        tuple: A tuple containing the solutions for part one 
+        and part two of the puzzle, respectively.
+    """
+
     numbers = parse(puzzle_input)
     solution_1 = part_one(numbers)
     solution_2 = part_two(numbers)
